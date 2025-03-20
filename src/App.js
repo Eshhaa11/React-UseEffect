45134188f50e09e963d58f1f3ea409834022ba11
Esha Patel <Patellesha11@gmail.com>
2025-03-20 17:44:19 +0300

    Fix fetch function to retrieve Pokémon data and handle errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import {useEffect, useState } from "react";
 function App() {
     const [data, setData] = useEffect([]);
 
-    const fetchPokemon = () =>
+    const fetchPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+            .then((response) => response.json())
+            .then((data) => setData(data));
+            .catch(error => console.log(error));
+    }
+
+    
 
 }
 
