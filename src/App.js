d6a78f90a5a06765b6fea31da63867394f160e9e
Esha Patel <Patellesha11@gmail.com>
2025-03-21 16:54:42 +0300

    Add pagination support to fetchPokemon function and implement handleNext for offset management

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ function App() {
   const [data, setData] = useState([]);
 
   const fetchPokemon = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    let url = `https://pokeapi.co/api/v2/pokemon`
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         console.log(data.results);
         setData(data.results);
       })
       .catch((error) => console.error("error fetching", error));
-  };
+  }
 
+  const handleNext = () => {
+    setOffset (prev => prev += 20);
+  }
+  
+  
   useEffect(() => {
     fetchPokemon();
   }, [data.length]);
