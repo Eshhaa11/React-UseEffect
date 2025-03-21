9c26706ee9b87b74e0e6ab6a83f96a5061ae74b4
Esha Patel <Patellesha11@gmail.com>
2025-03-21 16:39:43 +0300

    Fix fetchPokemon function to set data state and render Pokémon names in the App component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [data, setData] = useEffect([]);
+  const [data, setData] = useState([]);
 
-    const fetchPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.results);
-            })
-            .catch(error => console.error("error fetching" , error));
-    }
+  const fetchPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setData(data.results);
+      })
+      .catch((error) => console.error("error fetching", error));
+  };
 
-    useEffect(() => {
-        fetchPokemon();
-    }, [data.length])
+  useEffect(() => {
+    fetchPokemon();
+  }, [data.length]);
 
-    return (
-        <ol>
-             {data.map((val, key) => {
-                const { name } = val;
-             }}
-        </ol>
-      <div>
-
-
-      </div>
-    );
+  return (
+    <div>
+      <ol>
+        {data.map((val, key) => {
+          const { name } = val;
+          return <li key={key}>{name}</li>;
+        })}
+      </ol>
+    </div>
+  );
 }
 
 export default App;
-
-
